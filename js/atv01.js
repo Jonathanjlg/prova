5e301553a4e26697551d6a13054f2e99267fbc0c
Aluno <aluno@edu.senac.br>
2023-09-13 21:29:19 -0300

    testando

---
+++ js/atv01.js
@@ -0,0 +1,5 @@
+function SeuNome(){
+    let nome = document.querySelector("#nome").value
+    let resposta = document.querySelector("h2")
+    resposta.innerHTML = `Olar ${nome} Seja Bem Vindo!!!` 
+}
---
+++ js/atv02.js
@@ -0,0 +1,6 @@
+function OdobroNumber(){
+    let num1 = parseInt(document.getElementById("num1").value);
+    let multiplica = num1 *2
+   // alert(num1*2)
+   document.getElementById("resutado").innerHTML= (`o resutado e <br> ${multiplica} `)
+}
